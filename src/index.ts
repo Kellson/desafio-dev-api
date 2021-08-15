cfe32f21a4d9d8aa4e0cc75bc0e5d72ed8604e77
Kellson <kellson.lima@dsbsolucoes.com.br>
2021-08-15 17:12:38 -0300

    feat: cors adicionado a projeto

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 require('dotenv').config();
 import * as express from 'express';
 import 'mysql2';
+import * as cors from 'cors';
 import { OperacaoController } from './controllers/operacao';
 
 const app: express.Application = express();
 app.use(express.json());
+app.use(cors());
 app.use('/operacao', OperacaoController);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
